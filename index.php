ba8ae2250f586d849a7c31b7473c7158ae10ae00
David Baldwin <sourcedevelopers@protonmail.com>
2018-08-05 09:58:53 -0500

    update

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>My Portfolio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="#" />
+    <link rel="stylesheet" type="text/css" media="screen" href="###" />
     <script src="###"></script>
 </head>
 <body>
